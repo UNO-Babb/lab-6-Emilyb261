ab4950f931cc638e3a481badf683cbeeebb331d7
Emilyb261 <ebillings@unomaha.edu>
2025-10-01 14:50:36 -0500

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,32 @@
 #WordCount.py
-#Name:
+#Name: Emily Billings
 #Date:
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0 
+  charactersCount = 0
   
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1 
+    
+    for ch in line.strip("\n"): 
+      charactersCount +=1
+    
+    #print(line)
 
+    print("Lines:", lineCount)
+    print("Words:", wordCount)
+    print("characters:", charactersCount)
+    charactersCount +=1
+    
+    print("characters:", charactersCount)
+  
 if __name__ == '__main__':
   main()
