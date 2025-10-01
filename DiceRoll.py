dcc1d8d766b2fd122be03c2c1aee06e05ff1f8f7
Emilyb261 <ebillings@unomaha.edu>
2025-10-01 14:49:57 -0500

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,5 +1,5 @@
 #DiceRoll.py
-#Name:
+#Name: Emily Billings
 #Date:
 #Assignment:
 import random
@@ -7,12 +7,18 @@ import random
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  for r in range(100):
+    dice1 = random.randint(1,6) 
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
   #Create two dice values ranging from 1 - 6 each
   
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  dice = 1 
+  for count in rolls: 
+    print(dice, ":", count)
+    dice = dice + 1
 
 if __name__ == '__main__':
   main()
